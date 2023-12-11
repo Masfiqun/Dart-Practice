8090a4560f1cb883bcb7ab43c16ac8614ac7ec50
Masfiqun <128674394+Masfiqun@users.noreply.github.com>
2023-12-11 21:47:53 +0600

    loops through dart

---
+++ loops.dart
@@ -0,0 +1,39 @@
+void main(){
+  //loops
+
+  //for loop
+    // for(var i=0;i<100;i=i+10){
+
+    //     print("dart"+i.toString());
+
+    // }
+
+  //for...in loop
+    // var myNum=[5,2,6,4,123,52,69,85];
+
+    // // ignore: non_constant_identifier_names
+    // for(var OneNumber in myNum){
+    //   print(OneNumber);
+    // }
+
+
+  //while loop
+
+  // var i=0;
+  // while(i<100){
+  //   print(i);
+  //   i=i+10;
+  // }
+
+
+//do...while loop
+
+  // var i=0;
+
+  // do{
+  //   print(i);
+  //   i=i+15;
+  //  }while(i<100);
+
+
+}

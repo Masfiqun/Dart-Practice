d058572f52462f234882da0cd56667b113342a6d
Masfiqun <128674394+Masfiqun@users.noreply.github.com>
2023-12-12 01:27:09 +0600

    DART practice

---
+++ list & maps.dart
@@ -0,0 +1,49 @@
+void main(){
+
+//List
+  // var city=['Dhaka', 'Rangpur', 'Khulna', 'Barisal'];
+
+  // print(city[1]);
+
+
+//list
+  
+  // var city=['Daka', 'Rangpur', 'Rajshahi', 'Khulna'];
+
+  // var result= city.isNotEmpty;
+
+  // print(result);
+
+
+//list length
+  //growable length
+    // var city=['Daka', 'Rangpur', 'Rajshahi', 'Khulna'];
+
+    // print(city);
+
+    // city.add("Mymensingh");
+
+    // print(city);
+
+//maps
+  // var student = {'name':'Masfiqun', 'age':25, 'Branch':'Computer Science'};
+
+  // print(student['name']);
+
+//maps
+
+    // var productList=[
+    //   {'name':'sugar', 'price':50},
+    //   {'name':'milk', 'price':120},
+    //   {'name':'cake', 'price':300},
+    // ];
+
+    // for(var oneProduct in productList){
+
+    //   var item="Rpduct name is ${oneProduct['name']} and price is ${oneProduct['price'] } taka";
+    //   print(item);
+    // }
+
+
+
+}

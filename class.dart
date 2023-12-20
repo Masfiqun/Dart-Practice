0fa0d2abef257fa12c35713957d1e0ef196dc953
Masfiqun <128674394+Masfiqun@users.noreply.github.com>
2023-12-20 17:56:20 +0600

    Normal practice

---
+++ class.dart
@@ -0,0 +1,86 @@
+//functions wil be called through classes object
+// class MyClass{
+
+//   var MyName="Masfiqun";
+//   var Alphabet=['A', 'B', 'C'];
+
+//   addTwoNumber(int x , int y){
+//     print(x+y);
+//   }
+
+//   addThreeNumber(int x, int y, int z){
+//     print(x+y+z);
+//   }
+
+// }
+
+
+
+//if static is used before any function than the functions will be call through directly class
+// class MyClass{
+
+//   var MyName="Masfiqun";
+//   var Alphabet=['A', 'B', 'C'];
+
+//   static addTwoNumber(int x , int y){
+//     print(x+y);
+//   }
+
+//   addThreeNumber(int x, int y, int z){
+//     print(x+y+z);
+//   }
+
+// }
+
+
+//constructor
+// class MyClass{
+
+//   MyClass(String msg){
+//     print(msg);
+//   }
+// }
+
+
+
+// class MyClass{
+
+//   var num1=10;
+//   var num2=20;
+
+//   addTwoNumber(){
+//     var result=this.num1+this.num2;
+
+//     print(result);
+
+
+//     myFunction(){
+
+//       this.addTwoNumber();
+
+//     }
+
+    
+//   }
+// }
+
+
+
+
+
+// class Father{
+
+//   TotalMoney(){
+//     print("Total Amount = 8000");
+//   }
+
+// }
+
+// class Son extends Father{
+
+//   //overridding
+//   TotalMoney(){
+//     print("Total Amount = 5000");
+//   }
+
+// }
---
+++ dart_application_1.dart
@@ -0,0 +1,49 @@
+// void main(){
+  
+//   var obj= MyClass();
+
+//   // obj.addTwoNumber(20, 30);
+//   obj.addThreeNumber(5, 6, 10);
+//   MyClass.addTwoNumber(20,30);
+
+//   print(obj.MyName);
+//   print(obj.Alphabet[2]);
+// }
+
+//constructor
+  // void main(){
+
+  //   var obj=MyClass("i am a constructor parameter");
+
+
+  // }
+
+
+
+
+// import 'class.dart';
+
+// void main(){
+
+//   var obj=MyClass();
+
+//   obj.addTwoNumber();
+// }
+
+
+
+
+
+
+
+// import 'class.dart';
+
+// void main(){
+
+//   var SonObj=Son();
+//   SonObj.TotalMoney();
+
+
+//   var FatherObj=Father();
+//   FatherObj.TotalMoney();
+// }
